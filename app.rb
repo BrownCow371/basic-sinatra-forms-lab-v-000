fc89f0acdd8a64ecde3acfbd5b03e09a5ff328c1
Jamie Brown <jmbrowncow@gmail.com>
2018-08-08 18:02:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @point = params[:pg]
+    @shooting= params[:sg]
+    @small_for = params[:sf]
+    @power_for = params[:pf]
+    @center = params[:c]
     erb :team
   end
 end
